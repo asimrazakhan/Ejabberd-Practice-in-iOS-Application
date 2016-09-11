ae3a2253a1a3774c3337864d94854523dbc1a248
Asim Raza Khan <asim.r.k@outlook.com>
2016-09-11 19:42:49 +0500

    Passing the string value of 'Hello World' to the SecondViewController

--- xmppClientApp/xmppClientApp/SecondViewController.swift
+++ xmppClientApp/xmppClientApp/SecondViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var statusLabel: UILabel!
+    var status : String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        statusLabel.text = status
+        
         // Do any additional setup after loading the view.
     }
 
--- xmppClientApp/xmppClientApp/ViewController.swift
+++ xmppClientApp/xmppClientApp/ViewController.swift
@@ -19,6 +19,23 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "showSecondViewController" {
+            let secondViewController = segue.destinationViewController as! SecondViewController
+            secondViewController.status = "Hello World"
+        }
+    }
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
 
 
 }
